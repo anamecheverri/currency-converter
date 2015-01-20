2565dd6a850bf42d1326bd36414147e0ad69cfc9
Ana Maria Echeverri <ame@anamariaecheverri.com>
2015-01-20 13:43:20 -0500

    returns 0 if currencies not in tuple and validates conversion of a unit of a currency

--- currency_converter.py
+++ currency_converter.py
@@ -1,5 +1,17 @@
 
+def get_exchange_rate(rates, cu_from, cu_to):
+    exch_tuple = [conv_element
+                  for conv_element in rates
+                  if conv_element[0] == cu_from and conv_element[1] == cu_to]
+    if exch_tuple == []:
+        return 0
+    else:
+        return exch_tuple[0][2]
 
-def convert (rates, value, cu_from, cu_to):
+
+def convert(rates, value, cu_from, cu_to):
     if cu_from == cu_to:
-        return value    
+        return value
+    else:
+        exch_converter = get_exchange_rate(rates, cu_from, cu_to)
+        return exch_converter*value
--- test_currency.py
+++ test_currency.py
@@ -1,6 +1,16 @@
 from currency_converter import *
 
+
 rates = [("USD", "EUR", 0.74), ("EUR", "JPY", 145.949)]
 
+
+def test_get_exchange_rate():
+    assert get_exchange_rate(rates, "USD", "EUR") == 0.74
+    assert get_exchange_rate(rates, "EUR", "JPY") == 145.949
+    assert get_exchange_rate(rates, "USD", "JPY") == 0
+
+
 def test_convert():
-    assert convert(rates,1,"USD","USD")  == 1
+    assert convert(rates, 1, "USD", "USD") == 1
+    assert convert(rates, 1, "USD", "EUR") == 0.74
+    assert convert(rates, 1, "POU", "EUR") == 0
