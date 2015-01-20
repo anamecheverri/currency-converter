4def300f0755d8342fe25e2d5a32cf9d9cd14935
Ana Maria Echeverri <ame@anamariaecheverri.com>
2015-01-20 14:07:08 -0500

    added tests for USD to EUR, EUR to USD, EUR to JPY, and JPY to EUR

--- test_currency.py
+++ test_currency.py
@@ -8,8 +8,8 @@ def test_get_exchange_rate():
     assert get_exchange_rate(rates, "USD", "EUR") == 0.74
     assert get_exchange_rate(rates, "EUR", "JPY") == 145.949
     assert get_exchange_rate(rates, "USD", "JPY") == 0
-    assert get_exchange_rate(rates, "JPY", "EUR") == 1/145.949
-    assert get_exchange_rate(rates, "EUR", "USD") == 1/0.74
+    assert get_exchange_rate(rates, "JPY", "EUR") == 1 / 145.949
+    assert get_exchange_rate(rates, "EUR", "USD") == 1 / 0.74
 
 
 def test_convert():
@@ -18,3 +18,5 @@ def test_convert():
     assert convert(rates, 1, "POU", "EUR") == 0
     assert convert(rates, 2, "USD", "EUR") == 1.48
     assert convert(rates, 2, "EUR", "JPY") == 291.898
+    assert convert(rates, 2, "EUR", "USD") == 2 * 1 / 0.74
+    assert convert(rates, 2, "JPY", "EUR") == 2 * 1 / 145.949
